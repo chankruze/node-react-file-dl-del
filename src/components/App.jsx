4f6e08cdeaeda94da291e2f686ed2ff2503392e0
chankruze <chankruze@gmail.com>
2020-11-28 04:55:45 +0530

    Add Demo for File Download form Express Backend
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,93 @@ Created: Tue Nov 24 2020 07:04:05 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2020 and beyond
 */
 
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import styles from './App.module.sass';
 
+const handleDl = (torrentUrl) => {
+	// post req
+	const data = JSON.stringify({ torrentUrl });
+
+	const config = {
+		method: 'post',
+		url: 'http://localhost:3265/file/download',
+		headers: {
+			'x-cproxy-api-key': 'V1StGXR8_Z5jdHi6B-myT',
+			'Content-Type': 'application/json'
+		},
+		data: data
+	};
+
+	axios(config)
+		.then(({ data: { status, file } }) => {
+			if (status === 'ok') {
+				axios
+					.get(`http://localhost:3265/file/download`, {
+						params: { fileName: file },
+						headers: {
+							'x-cproxy-api-key': 'V1StGXR8_Z5jdHi6B-myT'
+						},
+						responseType: 'blob'
+					})
+					.then(({ data }) => {
+						const url = window.URL.createObjectURL(
+							new Blob([data], {
+								type: 'application/x-bittorrent'
+							})
+						);
+						const link = document.createElement('a');
+						link.href = url;
+						link.setAttribute('download', file);
+						document.body.appendChild(link);
+						link.click();
+
+						axios
+							.post(
+								`http://localhost:3265/file/delete`,
+								{
+									fileName: file
+								},
+								{
+									headers: {
+										'x-cproxy-api-key':
+											'V1StGXR8_Z5jdHi6B-myT',
+										'Content-Type': 'application/json'
+									}
+								}
+							)
+							.then(({ data }) => console.log(data))
+							.catch((err) => console.log(err));
+					})
+					.catch((err) => console.log(err));
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+	// get data
+
+	// save to device
+};
+
 const App = () => {
-	const [time, setTime] = useState(new Date().toLocaleTimeString());
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setTime(new Date().toLocaleTimeString());
-		}, 1000);
-
-		return () => clearInterval(interval);
-	}, []);
+	const [torrentUrl, setTorrentUrl] = useState('');
 
 	return (
 		<div className={styles.app}>
-			<h1>{time}</h1>
-			<p className={styles.desc}>App created using chankruze&apos;s minimal WBR setup</p>
+			<p>
+				https://yts.mx/torrent/download/5E8BC430960F5E1F48B0ACC2FCB607AB192D612E
+			</p>
+			<div>
+				<p>Torrent URL</p>
+				<input
+					type="text"
+					value={torrentUrl}
+					onChange={(e) => setTorrentUrl(e.target.value)}
+				/>
+			</div>
+			<button onClick={() => handleDl(torrentUrl)}>Download</button>
 		</div>
 	);
 };
